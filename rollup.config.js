11f05e3610f1b6b8489c7a547cfb05ce42fa8b6f
JP Slavinsky <jpslav@gmail.com>
2021-10-03 02:36:50 +0000

    better bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,21 @@
 // import babel from 'rollup-plugin-babel';
 // import eslint from 'rollup-plugin-eslint';
 // import resolve from 'rollup-plugin-node-resolve';
+
+
 import commonjs from 'rollup-plugin-commonjs';
 // import replace from 'rollup-plugin-replace';
 import { uglify } from "rollup-plugin-uglify";
+import ignore from "rollup-plugin-ignore"
+
+
 
 export default {
   input: "lib/main.js",
   output: {
     file: "dist/bundle.min.js",
     format: "cjs",
+    exports: "auto"
   },
   // dest: "dist/autopilot.min.js",
   // format: "iife",
@@ -22,6 +28,8 @@ export default {
     //   browser: true,
     // }),
     commonjs(),
+    ignore(['./patchedMath']),
+
     // eslint({
     //   exclude: ['src/styles/**'],
     // }),
